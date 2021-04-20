c6a00b48d0a852507821818aeb335abd44236bd7
Dell <sunkararam1166@gmail.com>
2021-04-20 12:55:48 +0530

    commented

--- src/test/java/Practice/RamSunkaraDemo/TestCase4455.java
+++ src/test/java/Practice/RamSunkaraDemo/TestCase4455.java
@@ -4,7 +4,7 @@ public class TestCase4455 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("New test cases automated here 4455");
+//		System.out.println("New test cases automated here 4455");
 	}
 
 }
